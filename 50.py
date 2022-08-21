4ec282b0068d7926cd6c9996184447ed392f184b
kavyavempati <111690173+kavyavempati@users.noreply.github.com>
2022-08-21 21:31:23 +0530

    Create 50.py

---
+++ 50.py
@@ -0,0 +1,11 @@
+Calculate power of a number using a while loop
+base = 3
+exponent = 4
+
+result = 1
+
+while exponent != 0:
+    result *= base
+    exponent-=1
+
+print("Answer = " + str(result))
